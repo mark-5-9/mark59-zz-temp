e0e2e313cc2773d566c247a32dd5bd8514d47401
Philip Webb <philip.webb@iag.com.au>
2022-04-12 17:20:24 +1000

    5 init

--- mark59-sample-dsls/src/main/java/com/mark59/seleniumDSL/pageElements/AjaxControls.java
+++ mark59-sample-dsls/src/main/java/com/mark59/seleniumDSL/pageElements/AjaxControls.java
@@ -11,7 +11,7 @@ import org.openqa.selenium.support.ui.WebDriverWait;
  * This code or very similar can be found at multiple web sites such as StackOverflow.  It's purpose it to 
  * check for a condition that means a triggered JQuery Ajax call has completed.
  * 
- * <p>While so useful function anymore with the decline of Ajax, this code does gives a template
+ * <p>While not so useful a function anymore with the decline of Ajax, this code does gives a template
  * about how to go about creating a WebDriverWait with a custom ExpectedCondition     
  *
  */
