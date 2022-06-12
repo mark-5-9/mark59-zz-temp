22de208f6a6d63f461b9567a120ed5bcac856b8a
Philip Webb <philip.webb@iag.com.au>
2022-06-12 23:19:19 +1000

    spring 2.7.0 secconfig

--- mark59-datahunter/src/test/java/com/mark59/datahunter/test/DataHunterWebApplicationTests.java
+++ mark59-datahunter/src/test/java/com/mark59/datahunter/test/DataHunterWebApplicationTests.java
@@ -2,19 +2,21 @@ package com.mark59.datahunter.test;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
-import org.springframework.boot.web.server.LocalServerPort;
 
 // ref: https://spring.io/guides/gs/testing-web/
 // @SpringBootTest(webEnvironment = WebEnvironment.DEFINED_PORT) // will use port 8081 
+// note @LocalServerPort has been deprecated/removed
 
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 @ActiveProfiles("h2mem")
 public class DataHunterWebApplicationTests    {
 	
-	@LocalServerPort
+	@Value("${local.server.port}")
 	private int port;	
+	
 	
 	@Test
 	public void checkDataHunterApplicationContextLoads() {
