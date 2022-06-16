6216b536067a7e3e195b0c537d8c66288a865692
Philip Webb <philip.webb@iag.com.au>
2022-06-16 13:43:49 +1000

    refine cdp test

--- mark59-results-splitter/src/test/java/com/mark59/results/splitter/test/ResultsSplitterTest.java
+++ mark59-results-splitter/src/test/java/com/mark59/results/splitter/test/ResultsSplitterTest.java
@@ -16,18 +16,27 @@
 
 package com.mark59.results.splitter.test;
 
+import static org.junit.Assert.assertNotEquals;
+
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 import javax.xml.parsers.ParserConfigurationException;
 
+import org.apache.commons.lang3.StringUtils;
 import org.xml.sax.SAXException;
 
 import com.mark59.results.splitter.ResultsSplitter;
+import com.opencsv.CSVReader;
+import com.opencsv.exceptions.CsvValidationException;
 
 import junit.framework.TestCase;
 
@@ -36,7 +45,7 @@ import junit.framework.TestCase;
  * @author Philip Webb
  * Written: Australian Winter 2019  
  * 
- * Hint: the assert is pretty basic, just use git (or whatever source control) to look for changes in the output files... 
+ * The asserts are pretty basic here .. todo: file content compares. 
  */
 public class ResultsSplitterTest     extends TestCase
 {
@@ -126,7 +135,7 @@ public class ResultsSplitterTest     extends TestCase
     }
     
     
-    public void testJmterResultsSplitByDatatypeTxnsAsCDPONLYTest() throws IOException, ParserConfigurationException, SAXException
+    public void testJmterResultsSplitByDatatypeTxnsAsONLYCDPTest() throws IOException, ParserConfigurationException, SAXException, CsvValidationException
     {
     	Files.deleteIfExists(Paths.get("./TESTDATA/MERGED_SPLIT_BY_DATAYPE/jmterResultsFileConvertedToCSV_CPU_UTIL.csv"));
     	Files.deleteIfExists(Paths.get("./TESTDATA/MERGED_SPLIT_BY_DATAYPE/jmterResultsFileConvertedToCSV_DATAPOINT.csv"));
@@ -147,11 +156,30 @@ public class ResultsSplitterTest     extends TestCase
 		resultsSplitter.clearOutputDirectory();     
 		actualSamplesCount=resultsSplitter.convert();
         
-		assertEquals("total", 32, actualSamplesCount);  						// 8 + 9 + 13 + 2 = 32
+		assertEquals("total", 33, actualSamplesCount);  						// 8 + 9 + 13 + 2 = 32
         assertEquals("_CPU_UTIL", 9, linecount(outdest+"_CPU_UTIL.csv")); 		// 1 header =>  8 datas 
         assertEquals("_DATAPOINT", 12, linecount(outdest+"_DATAPOINT.csv")); 	// 1 header + 2 <lf> => 9 datas
         assertEquals("_MEMORY", 14, linecount(outdest+"_MEMORY.csv"));  		// 1 header =>  13 datas 
-        assertEquals("TXN", 3, linecount(outdest+".csv"));        				// 1 header =>  2 datas 
+        assertEquals("TXN", 4, linecount(outdest+".csv"));        				// 1 header =>  2 datas 
+        
+        
+        // all txn lines should be cdp
+		FileReader fileReader = new FileReader(outdest+".csv");
+		CSVReader csvReader = new CSVReader(new BufferedReader(fileReader));  
+		String[] csvDataLineFields = csvReader.readNext();						// header
+		List<String> csvHeaderFieldsList = Arrays.asList(csvDataLineFields);
+		//System.out.println("csvHeaderFieldsList = " + csvHeaderFieldsList  );
+		int fieldPoslabel     		= csvHeaderFieldsList.indexOf("label");
+		int fieldPosdataType  		= csvHeaderFieldsList.indexOf("dataType");
+		csvDataLineFields = csvReader.readNext();						// 1st data line
+	   	while ( csvDataLineFields != null ) { 
+	   		// System.out.println("label = " + csvDataLineFields[fieldPoslabel]);
+	   		// System.out.println("type  = " + csvDataLineFields[fieldPosdataType]);
+	   		assertEquals("label should end with (CDP)", " (CDP)", StringUtils.right(csvDataLineFields[fieldPoslabel], 6));
+	   		assertEquals("should be CDP type", "CDP", csvDataLineFields[fieldPosdataType]);
+	   		csvDataLineFields = csvReader.readNext();
+	   	}
+	   	csvReader.close();
         
     	Files.delete(Paths.get("./TESTDATA/MERGED_SPLIT_BY_DATAYPE/jmterResultsFileConvertedToCSV_CPU_UTIL.csv"));
     	Files.delete(Paths.get("./TESTDATA/MERGED_SPLIT_BY_DATAYPE/jmterResultsFileConvertedToCSV_DATAPOINT.csv"));
@@ -161,7 +189,7 @@ public class ResultsSplitterTest     extends TestCase
     }
 
     
-    public void testJmterResultsSplitByDatatypeTxnsAsCDPHIDETest() throws IOException, ParserConfigurationException, SAXException
+    public void testJmterResultsSplitByDatatypeTxnsAsHIDECDPTest() throws IOException, ParserConfigurationException, SAXException, CsvValidationException
     {
     	Files.deleteIfExists(Paths.get("./TESTDATA/MERGED_SPLIT_BY_DATAYPE/jmterResultsFileConvertedToCSV_CPU_UTIL.csv"));
     	Files.deleteIfExists(Paths.get("./TESTDATA/MERGED_SPLIT_BY_DATAYPE/jmterResultsFileConvertedToCSV_DATAPOINT.csv"));
@@ -182,11 +210,29 @@ public class ResultsSplitterTest     extends TestCase
 		resultsSplitter.clearOutputDirectory();     
 		actualSamplesCount=resultsSplitter.convert();
         
-		assertEquals("total", 122, actualSamplesCount);  						
+		assertEquals("total", 121, actualSamplesCount);  						
         assertEquals("_CPU_UTIL", 9, linecount(outdest+"_CPU_UTIL.csv")); 		 
         assertEquals("_DATAPOINT", 12, linecount(outdest+"_DATAPOINT.csv")); 	
         assertEquals("_MEMORY", 14, linecount(outdest+"_MEMORY.csv"));  		 
-        assertEquals("TXN", 93, linecount(outdest+".csv"));        				 
+        assertEquals("TXN", 92, linecount(outdest+".csv"));
+        
+        // all txn lines should never be cdp
+		FileReader fileReader = new FileReader(outdest+".csv");
+		CSVReader csvReader = new CSVReader(new BufferedReader(fileReader));  
+		String[] csvDataLineFields = csvReader.readNext();						// header
+		List<String> csvHeaderFieldsList = Arrays.asList(csvDataLineFields);
+		//System.out.println("csvHeaderFieldsList = " + csvHeaderFieldsList  );
+		int fieldPoslabel     		= csvHeaderFieldsList.indexOf("label");
+		int fieldPosdataType  		= csvHeaderFieldsList.indexOf("dataType");
+		csvDataLineFields = csvReader.readNext();						// 1st data line
+	   	while ( csvDataLineFields != null ) { 
+	   		System.out.println("label = " + csvDataLineFields[fieldPoslabel]);
+	   		System.out.println("type  = " + csvDataLineFields[fieldPosdataType]);
+	   		assertNotEquals("label should not end with (CDP)", " (CDP)", StringUtils.right(csvDataLineFields[fieldPoslabel], 6));
+	   		assertNotEquals("should be not be CDP type", "CDP", csvDataLineFields[fieldPosdataType]);
+	   		csvDataLineFields = csvReader.readNext();
+	   	}
+	   	csvReader.close();
         
     	Files.delete(Paths.get("./TESTDATA/MERGED_SPLIT_BY_DATAYPE/jmterResultsFileConvertedToCSV_CPU_UTIL.csv"));
     	Files.delete(Paths.get("./TESTDATA/MERGED_SPLIT_BY_DATAYPE/jmterResultsFileConvertedToCSV_DATAPOINT.csv"));
@@ -196,8 +242,8 @@ public class ResultsSplitterTest     extends TestCase
     }
    
  
-    public void testJmterResultsSplitByDatatypeTxnsAsSHOWCDPTest() throws IOException, ParserConfigurationException, SAXException
-    {
+    public void testJmterResultsSplitByDatatypeTxnsAsSHOWCDPTest() throws IOException, ParserConfigurationException, SAXException {
+    	
     	Files.deleteIfExists(Paths.get("./TESTDATA/MERGED_SPLIT_BY_DATAYPE/jmterResultsFileConvertedToCSV_CPU_UTIL.csv"));
     	Files.deleteIfExists(Paths.get("./TESTDATA/MERGED_SPLIT_BY_DATAYPE/jmterResultsFileConvertedToCSV_DATAPOINT.csv"));
     	Files.deleteIfExists(Paths.get("./TESTDATA/MERGED_SPLIT_BY_DATAYPE/jmterResultsFileConvertedToCSV_MEMORY.csv"));
@@ -221,16 +267,14 @@ public class ResultsSplitterTest     extends TestCase
         assertEquals("_CPU_UTIL", 9, linecount(outdest+"_CPU_UTIL.csv")); 		 
         assertEquals("_DATAPOINT", 12, linecount(outdest+"_DATAPOINT.csv")); 	
         assertEquals("_MEMORY", 14, linecount(outdest+"_MEMORY.csv"));  		 
-        assertEquals("TXN", 95, linecount(outdest+".csv"));        				 
-        
-    	Files.delete(Paths.get("./TESTDATA/MERGED_SPLIT_BY_DATAYPE/jmterResultsFileConvertedToCSV_CPU_UTIL.csv"));
+        assertEquals("TXN", 95, linecount(outdest+".csv"));
+
+        Files.delete(Paths.get("./TESTDATA/MERGED_SPLIT_BY_DATAYPE/jmterResultsFileConvertedToCSV_CPU_UTIL.csv"));
     	Files.delete(Paths.get("./TESTDATA/MERGED_SPLIT_BY_DATAYPE/jmterResultsFileConvertedToCSV_DATAPOINT.csv"));
     	Files.delete(Paths.get("./TESTDATA/MERGED_SPLIT_BY_DATAYPE/jmterResultsFileConvertedToCSV_MEMORY.csv"));
     	Files.delete(Paths.get("./TESTDATA/MERGED_SPLIT_BY_DATAYPE/jmterResultsFileConvertedToCSV.csv"));
     	Files.delete(Paths.get("./TESTDATA/MERGED_SPLIT_BY_DATAYPE"));
     }
-
-    
     
     
 	public int linecount(String filename) {  // count includes header line
@@ -251,16 +295,6 @@ public class ResultsSplitterTest     extends TestCase
 		}
 		return lineCount;
 	}    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
     
     
 }
